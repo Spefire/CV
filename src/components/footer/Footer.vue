8e363bed08e733cb56fa1d97540d9547ef75fc79
Nicholas Brun <spefire@gmail.com>
2020-06-22 12:39:20 +0200

    Refactor(footer): Change footer icons, separator and add responsive

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,15 +1,20 @@
 <template>
-  <footer v-bind:class="{ 'legaced': isLegacy }">
-    <div>
-      © Copyrights
-      <b>Nicholas Brun</b>
+  <footer v-bind:class="{ legaced: isLegacy }">
+    <div class="footer-line">
+      <div>© Copyrights <b>Nicholas Brun</b></div>
+      <div class="footer-mobile">. Tous droits réservés.</div>
     </div>
-    <div>
-      Site réalisé
-      <a href="https://github.com/Spefire/TemplateCV" target="_blank">
-        <b>par mes soins</b>
-      </a> |
-      <router-link class="footer-legacy" to="mentions-legales">Mentions Légales</router-link>
+    <div class="footer-line">
+      <div class="footer-left footer-mobile">
+        Site réalisé
+        <a href="https://github.com/Spefire/TemplateCV" target="_blank">
+          <b>par mes soins</b>
+        </a>
+      </div>
+      <div class="nav-separator footer-mobile"></div>
+      <router-link class="footer-right" to="mentions-legales"
+        >Mentions Légales</router-link
+      >
     </div>
   </footer>
 </template>
